ac3315c6a0923e4bd867ff23f0217202f1b838e8
Anas LARIBI <anas.laribi95@gmail.com>
2025-03-11 06:22:04 -0400

    Footer Added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Steps from "../components/Steps";
 import Description from "../components/Description";
 import Testimonials from "../components/Testimonials";
+import GenerateButtons from "../components/GenerateButtons";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Steps />
       <Description />
       <Testimonials />
+      <GenerateButtons />
     </div>
   );
 };
